02dc37c3f579c027844c78dc0866432ee459f534
charlescaseymartin <charlescaseymartin@gmail.com>
2024-09-10 20:35:45 +0200

    (fix) Removed server constant

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is listening on port: ${port}...`);
   console.log('Press Ctrl+C to quit.');
 });
